3020244898d61a9a5b5edb63f2347c8ba7a628f8
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-27 18:59:12 +0000

    changed dict to new syntax w/o =>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,7 @@ class MoviesController < ApplicationController
   def index
     matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @list_of_movies = matching_movies.order( created_at: :desc )
 
     respond_to do |format|
       format.json do
@@ -22,7 +22,7 @@ class MoviesController < ApplicationController
   def show
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where( id: the_id )
 
     @the_movie = matching_movies.first
   end
@@ -43,21 +43,21 @@ class MoviesController < ApplicationController
   def edit
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where( id: the_id )
 
     @the_movie = matching_movies.first
   end
 
   def update
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where( id: the_id ).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
 
     if the_movie.valid?
       the_movie.save
-      redirect_to(movie_url(the_movie), { :notice => "Movie updated successfully."} )
+      redirect_to(movie_url(the_movie), notice: "Movie updated successfully." )
     else
       redirect_to(edit_movie_url(the_movie.id), alert: "Movie failed to update successfully.")
     end
@@ -65,10 +65,10 @@ class MoviesController < ApplicationController
 
   def destroy
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id ).first
 
     the_movie.destroy
 
-    redirect_to(movies_url, { :notice => "Movie deleted successfully."} )
+    redirect_to(movies_url, notice: "Movie deleted successfully." )
   end
 end
